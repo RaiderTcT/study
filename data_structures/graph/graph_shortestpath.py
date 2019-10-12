f29b36905518b8dab1e364c480340af451236981
Ulysses <38154485+RaiderTcT@users.noreply.github.com>
2019-10-12 14:18:36 +0800

    Update graph_shortestpath.py

--- data_structures/graph/graph_shortestpath.py
+++ data_structures/graph/graph_shortestpath.py
@@ -1,140 +1,297 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-@Description: 最短路径问题
-@Date: 2019-10-02 09:47:54
 @Author: Ulysses
-@LastEditTime: 2019-10-11 17:22:08
+@Date: 2019-09-24 09:01:00
+@Description: 图的基本实现
+@LastEditTime: 2019-10-11 08:16:24
 '''
-from data_structures.tree.prioqueue import PrioQueue
-from data_structures.graph.graph import GraphAL, inf
-from pprint import pprint
+from collections import defaultdict
 
-def dijkstra_shortest_paths(graph, v0):
-    """
-    DiJkstra单源点最短路径, 从一个点到其他各个点的最短路径
-    最短路径中前段也是最短路径：要记录V0到各顶点的最短路径，只需要记录V0到v的
-    前一个顶点
-    Args:
-        graph ([type]): 图
-        v0 ([type]): 源点
-    复杂度: 时间 O(|E|log|E|), 空间O(|E|)
-    """
-    vnum = graph.vertex_num()
-    assert 0 <= v0 < vnum
-    paths = [None] * vnum  # 记录最短路径 （前一顶点v', v0到v最短路径长度）
-    count = 0
-    # 初始队列  (v0经v到v'的最短已知长度, v, v前一顶点v')
-    cands = PrioQueue([(0, v0, v0)])
-    while count < vnum and not cands.is_empty():
-        plen, u, vmin = cands.dequeue()  # 取具有最小p值的边
-        if paths[vmin]:  # vmin在U里,最短路径已知，继续
-            continue
-        paths[vmin] = (u, plen)  # 记录最新确定的最短路径
-        for v, w in graph.out_edges(vmin):  # 经过u的vmin的邻接边
-            if not paths[v]:  # 未知的最短路径的顶点路径(端点一个在U一个在V-U)
-                cands.enqueue((plen + w, vmin, v))   # 队列中添加新的路径
-                # v0到v距离要更新, 可以
-        count += 1
-    return paths
+class GraphError(TypeError):
+    pass
+
+inf = float("inf")  # 无穷大，表示无
 
 
-def all_shortest_paths(graph):
-    """
-    任意顶点间最短路径 Floyd算法，基于图的邻接矩阵A
-    n个顶点的图 G = (V, E)
-    - 边(v, v')∈E, 顶点v到v'的路径, 长度为A[v][v'], 无边时看做长度为∞的直接路径
-    - v到v'的直接路径未必最短, 途径其他顶点的路径可能更短
-    - 检查和比较v到v'的可能经过的任何顶点的所有路径, 找出最短路径
-    递推生成一系列n*n方阵 Ak(0<=k<=n),Ak[i][j]表示从vi到vj的途径顶点可为v0,
-    v1,...,vk-1的最短路径长度
-    A0[i][j] = A[i][j],vi到vj不经过任何顶点的最短路径长度
-    Ak+1[i][j] = min{Ak[i][j], Ak[i][k]+Ak[k][j]}, 0<=k<=n-1
-    递推过程中除了[i][j],其他元素不变可以用一个方阵进行计算,可以直接赋值修改[i][j]
-    时间复杂度:O(|V|^3)
-    空间复杂度:O(|V|^2)
-    """
-    vnum = graph.vertex_num()
-    a = [[graph.get_edge(i, j) for j in range(vnum)]
-            for i in range(vnum)]  # 复制一个新的邻接矩阵
-    # 记录最短路径上的下一个顶点 -1 表示不经过
-    # vi到vj路径上vi的后继顶点
-    nvertex = [[-1 if a[i][j] == inf else j
-                for j in range(vnum)]
-                 for i in range(vnum)]
-# [[0, -1, 2, 3, -1, -1, -1],
-#  [0, 1, 2, -1, -1, 5, -1],
-#  [-1, 1, 2, -1, 4, 5, -1],
-#  [-1, -1, -1, 3, 4, -1, -1],
-#  [-1, -1, -1, -1, 4, -1, 6],
-#  [-1, -1, -1, -1, -1, 5, 6],
-#  [-1, -1, -1, -1, -1, -1, 6]]
-    # pprint(nvertex)
-    for k in range(vnum):
-        for i in range(vnum):
-            for j in range(vnum):
-                if a[i][j] > a[i][k] + a[k][j]:
-                    a[i][j] = a[i][k] + a[k][j]  # 修改最短距离
-                    nvertex[i][j] = nvertex[i][k]  # 修改最短路径上经过的顶点
-    return (a, nvertex)
+# 邻接矩阵表示
+class Graph:
+    def __init__(self, mat, un_conn=0):
+        """
+        构造图
+        Args:
+            mat (list): 邻接矩阵，嵌套list形式
+            un_conn (int, optional): 参数为无关联特殊值. Defaults to 0.
+
+        Raises:
+            ValueError: 矩阵不是方阵
+        """
+        vnum = len(mat)
+        for x in mat:
+            if len(x) != vnum:
+                raise ValueError("mat需要为方阵")
+        self._mat = [mat[i][:] for i in range(vnum)]  # 拷贝生成新的矩阵
+        self._vnum = vnum  # 顶点数目
+        self._unconn = un_conn
+        self._edges = defaultdict(list)
+
+    def vertex_num(self):
+        return self._vnum
+
+    def _invalid(self, v):
+        # 检查顶点下标是否非法
+        return 0 > v or v >= self._vnum
+
+    def add_vertex(self):
+        raise GraphError('不支持的操作')
+
+    def add_edge(self, vi, vj, val=1):
+        """添加vi到vj的边"""
+        if self._invalid(vi) or self._invalid(vj):
+            raise GraphError(f"顶点{vi} 或者 {vj}为不合法的顶点")
+        self._mat[vi][vj] = val
+        self._edges[vi] = self._out_edges(self._mat[vi], self._unconn)
+
+    def get_edge(self, vi, vj):
+        if self._invalid(vi) or self._invalid(vj):
+            raise GraphError(f"顶点{vi} 或者 {vj}为不合法的顶点")
+        return self._mat[vi][vj]
+
+    def out_edges(self, vi):
+        """获取顶点vi的出边表"""
+        if self._invalid(vi):
+            raise GraphError(f"顶点{vi}为不合法的顶点")
+        if vi not in self._edges:
+            self._edges[vi] = self._out_edges(self._mat[vi], self._unconn)
+        return self._edges[vi]
+
+    @staticmethod
+    def _out_edges(row, unconn):
+        edges = []
+        for i in range(len(row)):
+            if row[i] != unconn:
+                edges.append((i, row[i]))  # 出边 终点顶点，边的信息（权值）
+        return edges
+
+    def __str__(self):
+        return f"[\n" + ',\n'.join(map(str, self._mat)) + "\n]"\
+            + "\nUnconnected: " + str(self._unconn)
 
 
-def get_dis_path(graph, v_start, v_end):
-    """获取"""
-    if v_start not in range(graph.vertex_num()) or v_end not in range(graph.vertex_num()):
-        raise ValueError("超出范围")
-    path = []
-    a, next_vertex = all_shortest_paths(graph)
-    dis = a[v_start][v_end]
-    i = v_start
-    while True:
-        path.append(i)
-        i = next_vertex[i][v_end]
-        if i == -1 or i == v_end:
-            path.append(i)
-            break
-    return dis, path
+# 邻接表表示
+class GraphAL(Graph):
+    def __init__(self, mat=[], un_conn=0):
+        vnum = len(mat)
+        for x in mat:
+            if len(x) != vnum:
+                raise ValueError("mat需要为方阵")
+        # [每个顶点的出边表]
+        self._mat = [
+            Graph._out_edges(mat[i], un_conn) for i in range(vnum)
+        ]
+        self._vnum = vnum
+        self._unconn = un_conn
 
+    def add_vertex(self):
+        self._mat.append([])
+        num = self._vnum
+        self._vnum += 1
+        return num
 
-if __name__ == "__main__":
-    gmat = [
-        [0, inf, 5, 2, inf, inf, inf],
-        [11, 0, 4, inf, inf, 4, inf],
-        [inf, 3, 0, inf, 2, 7, inf],
-        [inf, inf, inf, 0, 6, inf, inf],
-        [inf, inf, inf, inf, 0, inf, 2],
-        [inf, inf, inf, inf, inf, 0, 3],
-        [inf, inf, inf, inf, inf, inf, 0],
+    def add_edge(self, vi, vj, val=1):
+        # val为unconn 表示删去这条边
+        if self._vnum == 0:
+            raise GraphError("图为空")
+        if self._invalid(vi) or self._invalid(vj):
+            raise GraphError(f"顶点{vi} 或者 {vj}为不合法的顶点")
+        row = self._mat[vi]
+        i = 0
+        while i < len(row):
+            if row[i][0] == vj:  # 找到终止顶点
+                self._mat[vi][i] = (vj, val)  # 删除
+                if val == self._unconn:
+                    self._mat[vi].pop(i)
+                return
+            if row[i][0] > vj:
+                break
+            i += 1
+        self._mat[vi].insert(i, (vj, val))  # 添加边
+
+    def get_edge(self, vi, vj):
+        if self._invalid(vi) or self._invalid(vj):
+            raise GraphError(f"顶点{vi} 或者 {vj}为不合法的顶点")
+        for i, val in self._mat[vi]:
+            if i == vj:
+                return val
+        return self._unconn
+
+    def out_edges(self, vi):
+        if self._invalid(vi):
+            raise GraphError(f"不合法的顶点 {vi}")
+        return self._mat[vi]
+
+# dict 形式表示
+class GraphDict(Graph):
+    def __init__(self, mat, un_conn=0):
+        self._vnum = len(mat)
+        self._unconn = un_conn
+        self._mat = {}
+        for i in range(len(mat)):
+            for j in range(len(mat[i])):
+                if mat[i][j] != un_conn:
+                    self._mat[(i, j)] = mat[i][j]
+
+    def add_vertex(self):
+        num = self._vnum
+        self._vnum += 1
+        self._mat.update({(num, num): 0})  # 添加到自身的边
+        return num
+
+    def add_edge(self, vi, vj, val=1):
+        # val为0 表示删去这条边
+        if self._vnum == 0:
+            raise GraphError("图为空")
+        if self._invalid(vi) or self._invalid(vj):
+            raise GraphError(f"顶点{vi} 或者 {vj}为不合法的顶点")
+        key = (vi, vj)
+        if key in self._mat:
+            if val == self._unconn:
+                self._mat.pop(key)
+        else:
+            self._mat[key] = val
+
+    def get_edge(self, vi, vj):
+        if self._invalid(vi) or self._invalid(vj):
+            raise GraphError(f"顶点{vi} 或者 {vj}为不合法的顶点")
+        return self._mat.get((vi, vj), None)
+
+    def out_edges(self, vi):
+        if self._invalid(vi) :
+            raise GraphError(f"顶点{vi}为不合法的顶点")
+        l = []
+        for key, val in self._mat.items():
+            if key[0] == vi:
+                l.append((key[1], val))
+        return l
+
+    def __str__(self):
+        return f"[\n" + ',\n'.join(map(str, self._mat.items())) + "\n]"\
+            + "\nUnconnected: " + str(self._unconn)
+
+if __name__ == '__main__':
+    gmat1 = [
+        [0, 0, 1],
+        [1, 0, 1],
+        [0, 1, 0]
     ]
-    g = GraphAL(gmat, inf)
-    for path in dijkstra_shortest_paths(g, 0):
-        print(path)
+    gmat2 = [
+        [0, 1, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 0, 1],
+        [0, 0, 1, 0]
+    ]
+    # 带权值
+    gmat3 = [
+        [0, inf, 6, 3, inf],
+        [11, 0, 4, inf, inf],
+        [inf, 3, 0, inf, 5],
+        [inf, inf, inf, 0, 5],
+        [inf, inf, inf, inf, 0],
+    ]
+    g1 = Graph(gmat1)
+    g2 = Graph(gmat2)
+    g3 = Graph(gmat3, inf)
+    print(g1)
+# [
+# [0, 0, 1],
+# [1, 0, 1],
+# [0, 1, 0]
+# ]
+# Unconnected: 0
+    print("-"*30)
+    print(g2)
+# [
+# [0, 1, 1, 0],
+# [1, 0, 1, 0],
+# [1, 1, 0, 1],
+# [0, 0, 1, 0]
+# ]
+# Unconnected: 0
+    for edge in g2.out_edges(0):  # a点 出表
+        print(edge)
+# (1, 1)
+# (2, 1)
+    g2.add_edge(0, 3, 1)  # 添加a到d的边
+    print()
+    for edge in g2.out_edges(0):  # a点 出表
+        print(edge)
+# (1, 1)
+# (2, 1)
+# (3, 1)
+    print("-"*30)
+    for edge in g3.out_edges(2):  # c 出表
+        print(edge)
+# (1, 3)
+# (2, 0)
+# (4, 5)
+    print("-"*30)
+    gal3 = GraphAL(gmat3, inf)
+    print(gal3)
+# [
+# [(0, 0), (2, 6), (3, 3)],
+# [(0, 11), (1, 0), (2, 4)],
+# [(1, 3), (2, 0), (4, 5)],
+# [(3, 0), (4, 5)],
+# [(4, 0)]
+# ]
+    gal3.add_edge(0, 2, inf)  # 删去 a -> c
+    print(gal3)
+# [
+# [(0, 0), (3, 3)],
+# [(0, 11), (1, 0), (2, 4)],
+# [(1, 3), (2, 0), (4, 5)],
+# [(3, 0), (4, 5)],
+# [(4, 0)]
+# ]
+    gal3.add_edge(0, 4, 3)  # 添加 a -> e
+    print(gal3)
+# [
+# [(0, 0), (3, 3), (4, 3)],
+# [(0, 11), (1, 0), (2, 4)],
+# [(1, 3), (2, 0), (4, 5)],
+# [(3, 0), (4, 5)],
+# [(4, 0)]
+# ]
+    gal3.add_vertex()  # 添加顶点f
+    gal3.add_edge(1, 5, 3)  # b -> f
+    gal3.add_edge(5, 2, 7)  # f -> c
+    gal3.add_edge(5, 4, 10)  # f -> e
+    print(gal3.out_edges(1))
+    print(gal3.out_edges(5))
+# [(0, 11), (1, 0), (2, 4), (5, 3)]
+# [(2, 7), (4, 10)]
 
-#  V0到各个顶点的前一个 顶点， 距离
-# (0, 0)
-# (2, 8)
-# (0, 5)
-# (0, 2)
-# (2, 7)
-# (1, 12)
-# (4, 9)
-    paths, next_vertex = all_shortest_paths(g)
-    pprint(paths)
-    pprint(next_vertex)
-# [[0, 8, 5, 2, 7, 12, 9],
-#  [11, 0, 4, 13, 6, 4, 7],
-#  [14, 3, 0, 16, 2, 7, 4],
-#  [inf, inf, inf, 0, 6, inf, 8],
-#  [inf, inf, inf, inf, 0, inf, 2],
-#  [inf, inf, inf, inf, inf, 0, 3],
-#  [inf, inf, inf, inf, inf, inf, 0]]
-#
-# [[0, 2, 2, 3, 2, 2, 2],
-#  [0, 1, 2, 0, 2, 5, 5],
-#  [1, 1, 2, 1, 4, 5, 4],
-#  [-1, -1, -1, 3, 4, -1, 4],
-#  [-1, -1, -1, -1, 4, -1, 6],
-#  [-1, -1, -1, -1, -1, 5, 6],
-#  [-1, -1, -1, -1, -1, -1, 6]]
-    print(get_dis_path(g, 2, 0))
-# (14, [2, 1, 0])
+    print("-"*30)
+    g3_dict = GraphDict(gmat3, inf)
+    print(g3_dict)
+# [
+# ((0, 0), 0),
+# ((0, 2), 6),
+# ((0, 3), 3),
+# ((1, 0), 11),
+# ((1, 1), 0),
+# ((1, 2), 4),
+# ((2, 1), 3),
+# ((2, 2), 0),
+# ((2, 4), 5),
+# ((3, 3), 0),
+# ((3, 4), 5),
+# ((4, 4), 0)
+# ]
+    print(g3_dict.out_edges(2))  # c出表
+# [(1, 3), (2, 0), (4, 5)]
+    g3_dict.add_vertex()
+    g3_dict.add_edge(5, 2, 3)
+    g3_dict.add_edge(5, 4, 7)
+    print(g3_dict.out_edges(5))
+# [(5, 0), (2, 3), (4, 7)]
